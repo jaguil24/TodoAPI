451ffbc57a29e7cd2358ba71acb55a7a0d52311d
jaguil24 <jaguil24@my.centennialcollege.ca>
2025-02-03 13:11:13 -0500

    Update Program.cs

--- TodoAPI/Program.cs
+++ TodoAPI/Program.cs
@@ -10,7 +10,7 @@ namespace TodoAPI
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+            // Add services to the container..
 
             builder.Services.AddControllers();
             builder.Services.AddDbContext<TodoContext>(opt =>opt.UseInMemoryDatabase("TodoList"));
